9c0c34b8a71da21df695d343cca22ee004a558aa
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2023-05-28 19:30:49 +0530

    BinarySearchTree class implementation

---
+++ BInarySearchTree/binarySearchTree.js
@@ -0,0 +1,22 @@
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor () {
+        this.root = null
+    }
+
+    isEmpty() {
+        return this.root === null;
+    }
+}
+
+
+let bst = new BinarySearchTree();
+console.log('Is tree empty? ', bst.isEmpty());
